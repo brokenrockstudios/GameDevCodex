44185b96cdd0270b08ad873f1430a8ef4b5eb366
Broken Rock Studios <140193794+brokenrockstudios@users.noreply.github.com>
2025-02-12 07:55:09 -0800

    Create Enum.h

---
+++ Rider/Templates/Enum.h
@@ -0,0 +1,14 @@
+$COPYRIGHT_LINE$
+
+#pragma once
+
+/**
+ * $END$
+ */
+UENUM(BlueprintType)
+enum class $CLASS_MODULE_API_MACRO$E$UNPREFIXED_CLASS_NAME$ : uint8
+{
+    SomeEnumValue UMETA(DisplayName = "SomeEnumValue"),
+	
+    $UNPREFIXED_CLASS_NAME$_MAX UMETA(Hidden)
+};
