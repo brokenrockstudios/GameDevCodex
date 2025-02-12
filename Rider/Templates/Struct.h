dd401c91b647bf5629e4f99b6f823fa1a73ba38b
Broken Rock Studios <140193794+brokenrockstudios@users.noreply.github.com>
2025-02-12 07:54:54 -0800

    Create Struct.h

---
+++ Rider/Templates/Struct.h
@@ -0,0 +1,16 @@
+$COPYRIGHT_LINE$
+
+#pragma once
+
+#include "CoreMinimal.h"
+$BASE_CLASS_INCLUDE_DIRECTIVE$
+
+/**
+ *
+ */
+USTRUCT(BlueprintType)
+struct $CLASS_MODULE_API_MACRO$F$UNPREFIXED_CLASS_NAME$
+{
+	GENERATED_BODY()
+
+};
